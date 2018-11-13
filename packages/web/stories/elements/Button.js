652216e5ef7ae550b0673c1733a804f086961491
pavel kruchina <pavel.kruchina@gmail.com>
2018-11-13 16:39:50 +0200

    feat: form elements

--- packages/web/stories/elements/Button.js
+++ packages/web/stories/elements/Button.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Button from '../../ui/elements/Button';
+import {
+  Button,
+  SuccessButton,
+  PrimaryButton,
+  SmallButton,
+  DangerButton,
+  WhiteBgButton,
+  WhiteButton
+} from '../../ui/elements/Button';
 
-storiesOf('elements/Button', module).add('default', () => <Button label={'Button'} />);
+storiesOf('elements/Button', module).add('Button', () => <Button>Button</Button>);
+storiesOf('elements/Button', module).add('SuccessButton', () => (
+  <SuccessButton>Button</SuccessButton>
+));
+storiesOf('elements/Button', module).add('PrimaryButton', () => (
+  <PrimaryButton>Button</PrimaryButton>
+));
+storiesOf('elements/Button', module).add('SmallButton', () => <SmallButton>Button</SmallButton>);
+storiesOf('elements/Button', module).add('DangerButton', () => <DangerButton>Button</DangerButton>);
+storiesOf('elements/Button', module).add('WhiteBgButton', () => (
+  <WhiteBgButton>Button</WhiteBgButton>
+));
+storiesOf('elements/Button', module).add('WhiteButton', () => <WhiteButton>Button</WhiteButton>);
