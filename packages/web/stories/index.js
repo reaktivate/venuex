3f18d11b9f2d8d1ed498964f5988f84d8b7e8265
pavel kruchina <pavel.kruchina@gmail.com>
2018-11-14 18:51:22 +0200

    feat: add monthPicker

--- packages/web/stories/index.js
+++ packages/web/stories/index.js
@@ -9,4 +9,5 @@ import './elements/form/Input';
 import './elements/form/Select';
 import './elements/form/Textarea';
 import './elements/form/DatePicker';
+import './components/Events/MonthPicker';
 import './components/Calendar';
