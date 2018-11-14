4069e629004b773a4902f1db5158c70aa02a31ac
pavel kruchina <pavel.kruchina@gmail.com>
2018-11-14 15:42:05 +0200

    feat: add textarea

---
+++ packages/web/stories/elements/form/Textarea.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Textarea from 'ui/elements/form/Textarea';
+
+const text = `Some texts
+on next line and something else
+ on next line and something else
+ on next line and something else
+ on next line and something else
+ on next line and something else
+ on next line and something else
+ 
+ on next line and something else
+ on next line and something else
+ on next line and something else
+ on next line and something elseon next line and something else
+ 
+ `;
+
+storiesOf('elements/form/Textarea', module)
+  .add('default', () => <Textarea label="some label">{text}</Textarea>)
+  .add('error', () => (
+    <Textarea label="some label" meta={{ error: 'some error', touched: true }}>
+      {text}
+    </Textarea>
+  ));
--- packages/web/stories/index.js
+++ packages/web/stories/index.js
@@ -2,4 +2,5 @@ import './elements/Button';
 import './elements/form/BaseInput';
 import './elements/form/Input';
 import './elements/form/Select';
+import './elements/form/Textarea';
 import './elements/form/DatePicker';
