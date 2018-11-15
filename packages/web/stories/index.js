73b28ad21d29ffb57c36a6920c4ca31d0c3dcf30
pavel kruchina <pavel.kruchina@gmail.com>
2018-11-15 16:59:25 +0200

    feat: add LegendItem

--- packages/web/stories/index.js
+++ packages/web/stories/index.js
@@ -11,4 +11,5 @@ import './elements/form/Select';
 import './elements/form/Textarea';
 import './elements/form/DatePicker';
 import './components/Events/MonthPicker';
+import './components/Events/LegendItem';
 import './components/Calendar';
