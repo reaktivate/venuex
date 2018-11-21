4d744c12a78259e1d8716d88374be1c604b12b0a
alex postnikov <postnikov@gmail.com>
2018-11-21 16:48:41 +0200

    feat: basic data model (needs code fixes probably)

--- packages/domain/models/Event.js
+++ packages/domain/models/Event.js
@@ -1,19 +1,49 @@
 import { observable } from 'mobx';
 import { Entity, DataTypes } from '@venuex/ddd';
 
+class Payment {
+  static schema = {
+    start: DataTypes.date.required,
+    amount: DataTypes.number,
+    paid: DataTypes.bool
+  };
+}
+
 class Event extends Entity {
   static schema = {
     ...Entity.schema,
+    venueId: DataTypes.string.required, // reference in fact
     name: DataTypes.string.required,
     // type: DataTypes.string.required,
     start: DataTypes.date.required,
-    end: DataTypes.date.required
+    end: DataTypes.date.required,
+    payments: DataTypes.arrayOf(Payment),
+    notes: DataTypes.string,
+    clientName: DataTypes.string,
+    clientId: DataTypes.string, // reference to client user, in future - NA now
+    room: DataTypes.string, // reference to venue.room
+    layout: DataTypes.string, // reference to venue.layout
+    minimumGuests: DataTypes.int,
+    consultants: DataTypes.arrayOf(string), // references to staff
+    owner: DataTypes.string, // reference to staff
+    ceremonyKind: DataTypes.string, // reference to venue.ceremonyKinds
+    actualGuests: DataTypes.int
   };
 
   @observable name;
   @observable type;
   @observable start;
   @observable end;
+  @observable payments;
+  @observable notes;
+  @observable room;
+  @observable clientName;
+  @observable clientId;
+  @observable layout;
+  @observable minimumGuests;
+  @observable consultants;
+  @osbervable owner;
+  @observable ceremonyKind;
 }
 
 export default Event;
